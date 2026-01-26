0ed3d8f2b5089124c01e7c710af23f89e29b0924
Jonathan Zermeno <141548012+Zermeno-Jonathan@users.noreply.github.com>
2026-01-25 22:47:18 -0600

    latest changes and additions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        /*A fragment is a React component that allows you to group multiple 
+      elements without adding an extra DOM node
+      it is used by placing the code inside a pair of empty tags (<></>)
+      the fragment works as a placeholder parent element*/
+        <>
+            <div>
+                <p>Hello World</p>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
