24b277fd87db877205780fdf0e6312f71ce36caf
Jonathan Zermeno <141548012+Zermeno-Jonathan@users.noreply.github.com>
2026-01-26 22:10:01 -0600

    indentation changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,9 @@ import './App.css';
 function App() {
     return (
         /*A fragment is a React component that allows you to group multiple 
-      elements without adding an extra DOM node
-      it is used by placing the code inside a pair of empty tags (<></>)
-      the fragment works as a placeholder parent element*/
+        elements without adding an extra DOM node
+        it is used by placing the code inside a pair of empty tags (<></>)
+        the fragment works as a placeholder parent element*/
         <>
             <div>
                 <p>Hello World</p>
