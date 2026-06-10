e061f4d92579a186cd5db0fd525dfb44f2477b76
Jonathan <jonathanzg.wd@gmail.com>
2026-06-10 00:12:11 -0600

    home.jsx/.css created and exported

---
+++ frontend/src/home.jsx
@@ -0,0 +1,11 @@
+import styles from './home.module.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <p className={styles.paragHome}>Home</p>
+        </div>
+    );
+}
+
+export default Home;
