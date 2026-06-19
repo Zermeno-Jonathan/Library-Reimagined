0d4a3bd4a3923889f6daa014b3919cb97c9e01d2
Jonathan <jonathanzg.wd@gmail.com>
2026-06-19 00:50:16 -0600

    feat: setup React Router with nested layouts and page structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,41 @@
-// import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Layouts
+import MainLayout from './components/layouts/mainLayout/MainLayout'
+import AuthLayout from './components/layouts/AuthLayout/AuthLayout'
+
 // Pages
-import Home from './pages/Home/Home';
-import Books from './pages/Books/Books';
-import Loans from './pages/Loans/Loans';
-import Login from './pages/Login/Login';
-import MenuAdmin from './pages/MenuAdmin/MenuAdmin';
-import MenuUser from './pages/MenuUser/MenuUser';
-import Queries from './pages/Queries/Queries';
-import Users from './pages/Users/Users';
-// import Test1 from './test1';
+import Home from './pages/Home/Home'
+import Books from './pages/Books/Books'
+import Loans from './pages/Loans/Loans'
+import Login from './pages/Login/Login'
+import MenuAdmin from './pages/MenuAdmin/MenuAdmin'
+import MenuUser from './pages/MenuUser/MenuUser'
+import Queries from './pages/Queries/Queries'
+import Users from './pages/Users/Users'
 
 function App() {
     return (
-        /*A fragment is a React component that allows you to group multiple 
-        elements without adding an extra DOM node
-        it is used by placing the code inside a pair of empty tags (<></>)
-        the fragment works as a placeholder parent element*/
-
         <BrowserRouter>
             <Routes>
-                {/* Pages */}
-                <Route path="/Home" element={<Home />} />
-                <Route path="/Books" element={<Books />} />
-                <Route path="/Loans" element={<Loans />} />
-                <Route path="/Login" element={<Login />} />
-                <Route path="/MenuAdmin" element={<MenuAdmin />} />
-                <Route path="/MenuUser" element={<MenuUser />} />
-                <Route path="/Queries" element={<Queries />} />
-                <Route path="/Users" element={<Users />} />
+                {/* Páginas con Header, Navbar, Footer */}
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/books" element={<Books />} />
+                    <Route path="/loans" element={<Loans />} />
+                    <Route path="/menu-admin" element={<MenuAdmin />} />
+                    <Route path="/menu-user" element={<MenuUser />} />
+                    <Route path="/queries" element={<Queries />} />
+                    <Route path="/users" element={<Users />} />
+                </Route>
+
+                {/* Páginas de autenticación sin Header/Navbar/Footer */}
+                <Route element={<AuthLayout />}>
+                    <Route path="/login" element={<Login />} />
+                </Route>
             </Routes>
         </BrowserRouter>
-    );
+    )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css';
+
+function Header() {
+    return (
+        <div className="b">
+            <p className={styles.b}>Header</p>
+            <p>Bienvenidos</p>
+        </div>
+    );
+}
+
+export default Header;
