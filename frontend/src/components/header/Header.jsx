3b20eb92056fef74c6287b4652403813055e754e
Jonathan <jonathanzg.wd@gmail.com>
2026-06-20 00:54:02 -0600

    feat: implement MainLayout with styled header and navigation

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -2,9 +2,17 @@ import styles from './Header.module.css';
 
 function Header() {
     return (
-        <div className="b">
-            <p className={styles.b}>Header</p>
-            <p>Bienvenidos</p>
+        <div className={styles.header}>
+            <a href="/">
+                <img
+                    className={styles.logo}
+                    src="/books_icon.png"
+                    alt="library icon"
+                    height={40}
+                    width={40}
+                />
+            </a>
+            <h2 className={styles.title}>Library Reimagined</h2>
         </div>
     );
 }
