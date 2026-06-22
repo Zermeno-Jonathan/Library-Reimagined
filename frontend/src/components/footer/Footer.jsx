5fd61195a3d75fc43d28bdfe4d2ce0255f232d83
Zermeno-Jonathan <jonathanzg.wd@gmail.com>
2026-06-21 19:57:42 -0600

    style: change footer content

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -3,7 +3,9 @@ import styles from './Footer.module.css';
 function Footer() {
     return (
         <div className={styles.footer}>
-            <p className={styles.pfooter}>Footer</p>
+            <p className={styles.pfooter}>
+                Library reimagined - Book System Administration
+            </p>
         </div>
     );
 }
