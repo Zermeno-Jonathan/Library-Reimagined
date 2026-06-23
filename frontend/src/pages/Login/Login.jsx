dde6f34b5a8d4552d1f5012a4c22b7a8ddffa0fa
Jonathan <jonathanzg.wd@gmail.com>
2026-06-22 23:37:32 -0600

    feat: setup Supabase client and authentication infrastructure

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,5 +1,7 @@
-import styles from './Login.module.css';
 import { useNavigate } from 'react-router-dom';
+// import { useEffect } from 'react';
+import supabase from '../../config/supabaseClient';
+import styles from './Login.module.css';
 
 function Login() {
     const navigate = useNavigate();
@@ -8,11 +10,14 @@ function Login() {
         navigate('/'); // va al home
     };
 
+    console.log(supabase); //Temporarily commented outy, remove this console.log
+
     return (
         <form>
             <div className={styles.loginContainer}>
                 <h3 className={styles.loginTitle}>Access your account</h3>
             </div>
+            <div className={styles.loginForm}></div>
 
             <button
                 className={styles.cancellBtn}
