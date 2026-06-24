1f9f5ffcf7dd05fd00ab77fcbed88d63f51434ac
Jonathan <jonathanzg.wd@gmail.com>
2026-06-24 00:34:56 -0600

    style: implement Login page UI with floating labels and responsive layout

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,29 +1,61 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import styles from './Login.module.css';
 
 function Login() {
-    // console.log(supabase);
-
     const navigate = useNavigate();
 
-    const handleCancel = () => {
-        navigate('/'); // va al home
+    const handleLogin = () => {
+        navigate('/Dashboard'); //Takes user to their specific dashboard page after login
     };
 
     return (
         <form>
             <div className={styles.loginContainer}>
-                <h3 className={styles.loginTitle}>Access your account</h3>
-            </div>
+                <div className={styles.divTitle}>
+                    <h3 className={styles.loginTitle}>Access your account</h3>
+                </div>
 
-            {/* Cancel button to go back to the home pag */}
-            <button
-                className={styles.cancellBtn}
-                type="button"
-                onClick={handleCancel}
-            >
-                Cancel
-            </button>
+                <div className={styles.formGroup}>
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder=" "
+                        className={styles.input}
+                    />
+                    <label htmlFor="email" className={styles.label}>
+                        Enter your email address
+                    </label>
+                </div>
+
+                <div className={styles.formGroup}>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder=" "
+                        className={styles.input}
+                    />
+                    <label htmlFor="password" className={styles.label}>
+                        Enter your password
+                    </label>
+                </div>
+
+                {/* Button to login */}
+                <button
+                    className={styles.loginButton}
+                    type="button"
+                    onClick={handleLogin}
+                >
+                    Login
+                </button>
+
+                <div className={styles.linksContainer}>
+                    {/* Link to home page */}
+                    <Link to="/">Go back</Link>
+
+                    {/* Link to Register form */}
+                    <Link to="/Register">Don't have an account?</Link>
+                </div>
+            </div>
         </form>
     );
 }
