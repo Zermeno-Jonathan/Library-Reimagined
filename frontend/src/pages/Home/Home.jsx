ef4f4766f4c9d6e7345d9e8a2cca1cffe70d8500
Jonathan <jonathanzg.wd@gmail.com>
2026-08-03 21:51:45 -0600

    feat (temp): logout function added at home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,22 @@
 import styles from './Home.module.css';
 
 function Home() {
+    // temporal logout
+    const handleLogout = (e) => {
+        e.preventDefault();
+        localStorage.removeItem('userRole');
+        window.location.href = '/';
+    };
+
     return (
         <div className="home">
             <p className={styles.welcome}>Home</p>
+            <p>
+                {/* temporal logout */}
+                <a href="#" onClick={handleLogout}>
+                    Logout (temp)
+                </a>
+            </p>
         </div>
     );
 }
