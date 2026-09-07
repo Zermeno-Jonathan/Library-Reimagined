15c52a49641af5f3d4ef60694c8c3eb8c126620e
Jonathan <jonathanzg.wd@gmail.com>
2026-09-06 18:11:45 -0600

    feat(pages): add MenuAdmin dashboard for admin links

--- frontend/src/pages/MenuAdmin/MenuAdmin.jsx
+++ frontend/src/pages/MenuAdmin/MenuAdmin.jsx
@@ -1,10 +1,64 @@
+import { Link } from 'react-router-dom';
 import styles from './MenuAdmin.module.css';
 
 function MenuAdmin() {
     return (
-        <div className="MA">
-            <p className={styles.MA}>MenuAdmin</p>
-        </div>
+        <main className={styles.pageWraper}>
+            <div className={styles.menuAdminContainer}>
+                <h3 className={styles.pageTitle}>Administration Menu</h3>
+                <div className={styles.menuAdminLinks}>
+                    <div>
+                        <h4>
+                            <Link
+                                to="/adminloans"
+                                className={styles.menuAdminLink}
+                            >
+                                Admin Loans
+                            </Link>
+                        </h4>
+                        <p className={styles.linkDescriptions}>
+                            - Administrate loans
+                        </p>
+                    </div>
+
+                    <div>
+                        <h4>
+                            <Link to="/books" className={styles.menuAdminLink}>
+                                Books
+                            </Link>
+                        </h4>
+                        <p className={styles.linkDescriptions}>
+                            - Administrate books
+                        </p>
+                    </div>
+
+                    <div>
+                        <h4>
+                            <Link to="/users" className={styles.menuAdminLink}>
+                                Users
+                            </Link>
+                        </h4>
+                        <p className={styles.linkDescriptions}>
+                            - Administrate users
+                        </p>
+                    </div>
+
+                    <div>
+                        <h4>
+                            <Link
+                                to="/queries"
+                                className={styles.menuAdminLink}
+                            >
+                                Queries
+                            </Link>
+                        </h4>
+                        <p className={styles.linkDescriptions}>
+                            - Monitor the queries
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 }
 
