d5dbf77b960d2a47c95598f8edf4e0cf63196369
Jonathan <jonathanzg.wd@gmail.com>
2026-09-06 18:09:01 -0600

    feat(pages): add Catalog page for book search

---
+++ frontend/src/pages/Catalog/Catalog.jsx
@@ -0,0 +1,11 @@
+import styles from './Catalog.module.css';
+
+function Catalog() {
+    return (
+        <div className="b">
+            <p className={styles.b}>Catalog</p>
+        </div>
+    );
+}
+
+export default Catalog;
