4f1cfb01913751d054df4713daa4801b2b865e03
Jonathan <jonathanzg.wd@gmail.com>
2026-09-06 18:09:34 -0600

    feat(pages): add AdminLoans page for loan management

---
+++ frontend/src/pages/AdminLoans/AdminLoans.jsx
@@ -0,0 +1,11 @@
+import styles from './AdminLoans.module.css';
+
+function AdminLoans() {
+    return (
+        <div className="b">
+            <p className={styles.b}>AdminLoans</p>
+        </div>
+    );
+}
+
+export default AdminLoans;
